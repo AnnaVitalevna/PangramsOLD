b3efead5d4e089253797ca4e57c698bc5dee6158
AnnaVitalevna <44719752+AnnaVitalevna@users.noreply.github.com>
2019-05-29 21:30:26 +0600

    Add files via upload

---
+++ src/org/Analys.java
@@ -0,0 +1,67 @@
+package org;
+
+import java.io.BufferedReader;
+import java.io.FileInputStream;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.ArrayList;
+import java.util.Scanner;
+
+public class Analys {
+    
+    private Scanner scan = new Scanner(System.in);
+    private String maxString = "";
+    private int maxLength = 0;
+    
+    public void TheAnalysisOfSupply(){
+        
+        String FileName = "src\\doc\\" + scan.nextLine() + ".txt";
+        ArrayList<String> alfabet = new ArrayList<>();
+
+        try{
+            FileInputStream stream = new FileInputStream(FileName);
+            BufferedReader br = new BufferedReader(new InputStreamReader(stream));
+            String strLine;
+            while ((strLine = br.readLine()) != null){
+                String result = strLine.replaceAll("[\\-\\+\\.\\^:,\\(\\)\\ \\!\\?]","").toUpperCase();
+                for(int i = 0;i<result.length();i++) {
+                    if(!alfabet.contains(Character.toString(result.charAt(i)))){
+                        alfabet.add(Character.toString(result.charAt(i)));
+                    }
+                }
+                if(maxLength<alfabet.size()){
+                    maxString = strLine;
+                    maxLength = alfabet.size();
+                }
+                alfabet.clear();
+            }
+        }
+        catch(IOException e){
+            System.out.println("Ошибка");
+        }
+    }
+    
+    public void ComparisonWithPangrams(){
+        
+        try{
+            FileInputStream stream = new FileInputStream("src\\doc\\Pangrams.txt");
+            BufferedReader br = new BufferedReader(new InputStreamReader(stream));
+            ArrayList<String> alfabet = new ArrayList<>();
+            System.out.println("Предложение '" + maxString + "' из Текста существует, с " + maxLength + " уникальных символов.");
+            String strLine;
+            while ((strLine = br.readLine()) != null) {
+                String result = strLine.replaceAll("[\\-\\+\\.\\^:,\\(\\)\\ \\!\\?]","").toUpperCase();
+                for(int i = 0;i<result.length();i++) {
+                    if(!alfabet.contains(Character.toString(result.charAt(i)))){
+                        alfabet.add(Character.toString(result.charAt(i)));
+                    }
+                }
+                System.out.println("Панграмма '" + strLine + "' самое большое, с " + alfabet.size() + " уникальных символов." );
+                alfabet.clear();
+            }
+        }
+        catch(IOException e){
+            System.out.println("Ошибка");
+        }
+    }
+}
---
+++ src/org/Pangrams.java
@@ -0,0 +1,11 @@
+package org;
+
+public class Pangrams {
+
+    public static void main(String[] args) {
+        
+        Analys analys = new Analys();
+        analys.TheAnalysisOfSupply();
+        analys.ComparisonWithPangrams();
+    }
+}
